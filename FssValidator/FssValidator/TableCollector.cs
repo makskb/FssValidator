aeab5d466a1acef2438c5a6f390609b56946d8a7
mak <mak@skbkontur.ru>
2016-04-22 09:47:27 +0500

    validator

--- FssValidator/FssValidator/TableCollector.cs
+++ FssValidator/FssValidator/TableCollector.cs
@@ -10,9 +10,12 @@ using static FssValidator.Settings;
 namespace FssValidator
 {
     //тут будем собирать свойства каждой таблицы
-    public class TableCollector
+    public class Section
     {
-        public static List<int> Table
+        public int Number { get; set; }
+        public List<string> Rows { get; set; }
+        public List<string> Cell { get; set; } 
+        /*public static List<int> Table
         {
             get { return Template.Root?.Element("sections")?.Elements("section").Attributes("code").Select(x =>
             {
@@ -20,6 +23,36 @@ namespace FssValidator
                 int.TryParse(x.Value, out i);
                 return i;
             }).ToList(); }
+        }*/
+    }
+
+    internal class GetProperties
+    {
+        public static List<Section> PropertiesSections()
+        {
+            var section = new Section();
+            var allNumberSections = Template.Root?.Element("sections")?.Elements("section").Attributes("code").Select(x =>
+            {
+                int i;
+                int.TryParse(x.Value, out i);
+                return i;
+            }).ToList();
+            foreach (var numSection in allNumberSections)
+            {
+                section.Number = numSection;
+                /*section.Rows =
+                    Template.Root.Elements("sections")
+                        .Elements("section")
+                        .Where(x =>
+                        {
+                            int i;
+                            var t = x.Attribute("code").Value;
+                            int.TryParse(t, out i);
+                            if (i == numSection)
+                                return true;
+                            return false;
+                        });*/
+            }
         }
     }
-}
+} 
